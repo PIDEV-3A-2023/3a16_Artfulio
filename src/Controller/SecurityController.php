118eeb7305495738e765d3cc2f81c101b910d0e8
Armi64bit <bahaeddine.bouzid@esprit.tn>
2023-04-29 21:44:40 -1100

    log

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,8 +25,10 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout(): Response
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->redirectToRoute('app_artwork_index');
+
     }
 }
